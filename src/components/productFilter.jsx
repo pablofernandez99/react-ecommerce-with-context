b1c11ca1def248ac324b09bafc55009a48ffefbd
pablo fernandez <pablofernandezc99@gmail.com>
2024-10-14 00:16:40 -0300

    Cambios para filtrar productos

--- src/components/productFilter.jsx
+++ src/components/productFilter.jsx
@@ -5,6 +5,11 @@ import { getCategories } from "../utils/helper"
 const productFilter = ({ filterProducts }) => {
 
     const [categories, setCategories] = useState([])
+    const [isFilter, setIsFilter] = useState(false)
+
+    const toggleFilterMenu = () => {
+        setIsFilter(!isFilter)
+    }
 
     useEffect(() => {
         const allCategories = getCategories()
@@ -12,17 +17,27 @@ const productFilter = ({ filterProducts }) => {
     }, [])
 
     return (
-        <ul className="flex flex-col md:flex-row gap-3 justify-center mb-6 text-center max-w-72 mx-auto">
-            {categories.map((category, index) => (
-                <li
-                    key={index}
-                    className="bg-blue-500 text-white px-3 py-2 rounded cursor-pointer"
-                    onClick={() => filterProducts(category)}
-                >
-                    {category}
-                </li>
-            ))}
-        </ul>
+        <>
+            <button
+                className="border-2 border-blue-900 px-3 py-2 rounded text-gray-600 font-semibold hover:bg-blue-900 hover:text-white"
+                onClick={toggleFilterMenu}
+            >
+                Filter Products
+            </button>
+            {isFilter && (
+                <ul className="flex flex-row flex-wrap md:flex-nowrap gap-3 mb-6 mt-2 text-center max-w-72">
+                    {categories.map((category, index) => (
+                        <li
+                            key={index}
+                            className="bg-blue-500 text-white px-2 py-1 rounded cursor-pointer"
+                            onClick={() => filterProducts(category)}
+                        >
+                            {category}
+                        </li>
+                    ))}
+                </ul>
+            )}
+        </>
     )
 }
 
